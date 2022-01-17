d9575ef2733514ca4d21c09c23ba29df63495e91
iGeralt <mauryasujeet28@gmail.com>
2022-01-17 17:49:33 +0530

    cryptocurrencies page done

--- crypto-app/src/components/HomePage.jsx
+++ crypto-app/src/components/HomePage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import {Cryptocurrencies, News} from './'
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 
 const HomePage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
 
   const globalStats = data?.data?.stats;
 
@@ -49,6 +50,24 @@ const HomePage = () => {
           />
         </Col>
       </Row>
+      <div>
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified/>
+      <div>
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/News">Show More</Link>
+        </Title>
+      </div>
+      <News simplified/>
     </>
   );
 };
